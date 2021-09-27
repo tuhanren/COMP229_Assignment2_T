5f71e4b234db1dd6a33d320787569b20ab535452
Hanren Tu <tu.hanren@gmail.com>
2021-09-27 10:55:21 -0400

    TT frame added, try heroku

--- Server/Routes/index.js
+++ Server/Routes/index.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const fs_1 = __importDefault(require("fs"));
 const router = express_1.default.Router();
 exports.default = router;
 router.get("/", function (req, res, next) {
@@ -12,8 +13,12 @@ router.get("/", function (req, res, next) {
 router.get("/home", function (req, res, next) {
     res.render("index", { title: "Home" });
 });
-router.get("/about", function (req, res, next) {
-    res.render("index", { title: "About" });
+router.get('/about', function (req, res, next) {
+    let resumePath = 'Public/Assets/docs/resume.pdf';
+    fs_1.default.readFile(resumePath, function (err, data) {
+        res.contentType('application/pdf');
+        res.send(data);
+    });
 });
 router.get("/projects", function (req, res, next) {
     res.render("index", { title: "Projects" });
