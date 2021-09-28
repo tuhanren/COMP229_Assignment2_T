3e3143b96b5997be11d9c39df7c36303878fac51
Hanren Tu <tu.hanren@gmail.com>
2021-09-28 18:45:28 -0400

    lottiehome added

--- .history/public/Scripts/app_20210927025616.ts
+++ .history/public/Scripts/app_20210927025616.ts
@@ -1,9 +1,3 @@
-/* 
-File name: portfolio
-Student Name: Tu Hanren
-Student ID: 301061529
-Date: 2021-09-28
-*/
 // IIFE -- Immediately Invoked Function Expression
 (function () {
   function Start() {
