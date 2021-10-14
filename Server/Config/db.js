dad6d0e70fe6f09762f3e373dc9e759abd7c415a
TuHanren <tu.hanren@gmail.com>
2021-10-14 19:29:43 -0400

    updated db config

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Seceret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
 exports.LocalURI = "mongodb://localhost/store";
-exports.RemoteURI = "mongodb+srv://tuhanren:4JDCZTLE4pUjvgoy@comp229-f2021.xxij1.mongodb.net/store?retryWrites=true&w=majority";
+exports.RemoteURI = process.env.RemoteURI;
 exports.HostName = "remotehost";
 exports.Seceret = "somesecert";
 //# sourceMappingURL=db.js.map
--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,4 +1,5 @@
 export const LocalURI = "mongodb://localhost/store";
-export const RemoteURI = "mongodb+srv://tuhanren:4JDCZTLE4pUjvgoy@comp229-f2021.xxij1.mongodb.net/store?retryWrites=true&w=majority";
+export const RemoteURI = process.env.RemoteURI;
 export const HostName = "remotehost";
 export const Seceret = "somesecert";
+//export const RemoteURI = "mongodb+srv://tuhanren:4JDCZTLE4pUjvgoy@comp229-f2021.xxij1.mongodb.net/store?retryWrites=true&w=majority";
