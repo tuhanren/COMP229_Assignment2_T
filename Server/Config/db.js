2c158ce1fbef0f8351a7a18f5de3ca3d61511368
TuHanren <tu.hanren@gmail.com>
2021-10-16 12:15:53 -0400

    heroku config vars updated

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Seceret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
 exports.LocalURI = "mongodb://localhost/contactlist";
-exports.RemoteURI = "mongodb+srv://tuhanren:GvCqEUPYqGYHj2fm@comp229-assignment2.xxij1.mongodb.net/contactlist?retryWrites=true&w=majority";
+exports.RemoteURI = process.env.RemoteURI;
 exports.HostName = "remotehost";
 exports.Seceret = "somesecert";
 //# sourceMappingURL=db.js.map
--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,8 +1,6 @@
 export const LocalURI = "mongodb://localhost/contactlist";
-export const RemoteURI = "mongodb+srv://tuhanren:GvCqEUPYqGYHj2fm@comp229-assignment2.xxij1.mongodb.net/contactlist?retryWrites=true&w=majority"
+export const RemoteURI = process.env.RemoteURI;
 export const HostName = "remotehost";
 export const Seceret = "somesecert";
-//export const RemoteURI = "mongodb+srv://tuhanren:4JDCZTLE4pUjvgoy@comp229-f2021.xxij1.mongodb.net/store?retryWrites=true&w=majority";
+//export const RemoteURI = "mongodb+srv://tuhanren:GvCqEUPYqGYHj2fm@comp229-assignment2.xxij1.mongodb.net/contactlist?retryWrites=true&w=majority";
 //export const RemoteURI = process.env.RemoteURI;
-//mongodb+srv://tuhanren:GvCqEUPYqGYHj2fm@comp229-assignment2.xxij1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-//"mongodb+srv://tuhanren:4JDCZTLE4pUjvgoy@comp229-f2021.xxij1.mongodb.net/contactlist?retryWrites=true&w=majority"
